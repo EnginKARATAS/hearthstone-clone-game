0452a0b408839604abaec62274578a20d93d9bca
Engin Karatas <enginkaratas99@gmail.com>
2024-11-01 20:28:45 +0300

    improvement draw card counter

--- src/components/features/UI/YourTurn/YourTurn.jsx
+++ src/components/features/UI/YourTurn/YourTurn.jsx
@@ -1,14 +1,17 @@
 import "./YourTurn.css";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import GameConstants from "../../../GameConstants";
-
+import { drawCard, syncCardBaseLenght } from "../../hand/handSlice";
 export default function YourTurn() {
   const isClientTurn = useSelector((state) => state.counter.isClientTurn);
   const gameState = useSelector((state) => state.game.gameState);
+  const dispatch = useDispatch();
   const [openPopup, setOpenPopup] = useState(false);
   useEffect(() => {
     if (isClientTurn === true && gameState === "playing") {
+      dispatch(drawCard({ isEnemy: false }));
+      dispatch(syncCardBaseLenght());
       setOpenPopup(true);
       const timer = setTimeout(() => {
         setOpenPopup(false);
