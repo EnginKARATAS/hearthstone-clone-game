87d2dd6ba811d8263a4eef6a0b1753611c17b5c4
Engin Karatas <enginkaratas99@gmail.com>
2024-11-02 14:34:19 +0300

    create rotate screen for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import GameBoard from "./components/GameBoard";
 import "./App.css";
 import "./styles/fonts.css";
+import RotateScreen from "./components/features/Menu/RotateScreen";
+import { useEffect } from "react";
 function App() {
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+  useEffect(() => {
+    console.log(windowHeight);
+    console.log(windowWidth);
+  }, [windowHeight, windowWidth]);
+
+  if (windowWidth <= windowHeight) return <RotateScreen />;
+
   return (
     <>
       <div className="bg">
-        <GameBoard className="board"/>
+        <GameBoard className="board" />
       </div>
     </>
   );
---
+++ src/components/features/Menu/RotateScreen.jsx
@@ -0,0 +1,12 @@
+import "./RotateScreen.css";
+
+export default function RotateScreen() {
+  return (
+    <div className="rotate-screen">
+      <div className="rotate-screen-content">
+        <h1>İlk önce ekranı çevirmen lazım</h1>
+        <img width="100" src="/menu/menu/rotate.png" alt="rotate device" />
+      </div>
+    </div>
+  );
+}
