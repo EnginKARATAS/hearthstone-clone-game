420365f3d97f97bb6f41ced8bd49e44c77ba74db
Engin Karatas <enginkaratas99@gmail.com>
2024-11-03 11:20:39 +0300

    set character selecting algorithm

--- src/components/features/Menu/CharacterSelectionScreen/CharactersInComponent/CharactersInComponent.jsx
+++ src/components/features/Menu/CharacterSelectionScreen/CharactersInComponent/CharactersInComponent.jsx
@@ -1,13 +1,26 @@
-export default function CharactersInComponent({ characterPack, handleCharacterPackChange }) {
+export default function CharactersInComponent({ characterPack, handleCharacterPackChange, characters }) {
+  console.log(characters)
+  console.log(characterPack)
+  const handleBorder = (characterPack) => {
+    if (characters[0] === characterPack) {
+      return "1px solid white";
+    } else if (characters[1] === characterPack) {
+      return "1px solid white";
+    }
+    return "none";
+  };
   return (
     <div onClick={() => handleCharacterPackChange(characterPack)}>
       <img
         width="50"
+        style={{
+          border: handleBorder(characterPack),
+        }}
         src={
           "/cards/card-images/" + characterPack + "/" + characterPack + ".png"
         }
-        alt={characterPack}
-      />
+          alt={characterPack}
+          />
     </div>
   );
 }
