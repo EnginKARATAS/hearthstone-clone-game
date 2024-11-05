f9c742d225dc766b0401e07037bfe0e16dee897f
Engin Karatas <enginkaratas99@gmail.com>
2024-11-05 14:52:20 +0300

    update end game screen design

--- src/components/features/UI/GameManagement/EndGameScreen.jsx
+++ src/components/features/UI/GameManagement/EndGameScreen.jsx
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
-
+import { useTranslation } from "react-i18next";
 export default function EndGameScreen() {
+  const {t} = useTranslation();
   return (
     <div className="loading-screen fixed flex justify-center items-center flex-col">
       <h2 className="text-3xl">Game Over</h2>
       <span className="text-sm mb-5">
-        Wait for 7 seconds to go to secret level⬅
+        {t("waitFor7SecondsToGoToSecretLevel")}
       </span>
       <p className="loading text-xl  w-[500px] text-center">
-        The game has come to an end. Thank you for 
-        sending me the card packages(15 X2 cards!).
+        {t("theGameHasComeToAnEnd")}. {t("thankYouFor")}
+        <br />
+        {t("sendingMeTheCardPackages")}.
       </p>
-      <ul>
-        <li>Yavuz Çeliker</li>
-        <li>Mustafa Özkan</li>
-        <li>XXX</li>
-        <li>XXX</li>
-        <li>XXX</li>
-        <li>XXX</li>
+      <ul className=" mt-5">
+        <li className="bg-white p-2 mt-1">{t("yavuzCeliker")}</li>
+        <li className="bg-white p-2 mt-1">{t("mustafaOzkan")}</li>
+        <li className="bg-white p-2 mt-1">{t("xxx")}</li>
+        <li className="bg-white p-2 mt-1">{t("yyy")}</li>
+        <li className="bg-white p-2 mt-1">{t("ttt")}</li>
+        <li className="bg-white p-2 mt-1">{t("zzz")}</li>
       </ul>
     </div>
   );
