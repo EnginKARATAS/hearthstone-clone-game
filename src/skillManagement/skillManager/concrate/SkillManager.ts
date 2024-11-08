6c20e27582c6a729bca5d09ef19c48eeb48d2e89
Engin Karatas <enginkaratas99@gmail.com>
2024-11-08 23:38:33 +0300

    test vercel

--- src/skillManagement/skillManager/concrate/SkillManager.ts
+++ src/skillManagement/skillManager/concrate/SkillManager.ts
@@ -3,13 +3,13 @@ import { Castable } from "../../cast/abstract/Castable.ts";
 import { HealAllCards } from "../../cast/spells/concrates/heal/HealAllCards.ts";
 import { HealHero } from "../../cast/spells/concrates/heal/HealHero.ts";
 import { HealCard } from "../../cast/spells/concrates/heal/HealCard.ts";
+import { HealRandomCard } from "../../cast/spells/concrates/heal/HealRandomCard.ts";
 import { Skillable } from "../abstract/Skillable.ts";
 import { DealAllEnemyMinions } from "../../cast/spells/concrates/damage/DealAllEnemyMinions.ts";
 import { DealAll } from "../../cast/spells/concrates/damage/DealAll.ts";
 import { DealAllEnemy } from "../../cast/spells/concrates/damage/DealAllEnemy.ts";
 import { DealRandomEnemy } from "../../cast/spells/concrates/damage/DealRandomEnemy.ts";
 import { EmpowerRandomCard } from "../../cast/spells/concrates/mixed/EmpowerRandomCard.ts";
-import { HealRandomCard } from "../../cast/spells/concrates/heal/HealRandomCard.ts";
 import { GainArmor } from "../../cast/spells/concrates/armor/GainArmor.ts";
 import { GainHealthAmountOfCardsInYourHand } from "../../cast/spells/concrates/heal/GainHealthAmountOfCardsInYourHand.ts";
 
@@ -19,14 +19,14 @@ function createSkill(skillData: {
 }): Castable | null {
   console.log(skillData);
   switch (skillData.type) {
-    case "HealRandomCard":
-      return new HealRandomCard(skillData.amount!);
     case "HealCard":
       return new HealCard(skillData.amount!);
     case "HealHero":
       return new HealHero(skillData.amount!);
     case "HealAllCards":
       return new HealAllCards(skillData.amount!);
+    case "HealRandomCard":
+      return new HealRandomCard(skillData.amount!);
     case "DealRandomEnemy":
       return new DealRandomEnemy(skillData.amount!);
     case "DealAllEnemyMinions":
@@ -37,8 +37,8 @@ function createSkill(skillData: {
       return new DealAll(skillData.amount!);
     case "EmpowerRandomCard":
       return new EmpowerRandomCard(skillData.amount!);
-      case "GainArmor":
-        return new GainArmor(skillData.amount!);  
+    case "GainArmor":
+      return new GainArmor(skillData.amount!);
     case "GainHealthAmountOfCardsInYourHand":
       return new GainHealthAmountOfCardsInYourHand();
     default:
