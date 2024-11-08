f5be3700e9466bc3d9344f29f5f61407de461aad
Engin Karatas <enginkaratas99@gmail.com>
2024-11-08 22:33:31 +0300

    create credits screen internalization

--- src/components/features/Menu/Credits/Credits.jsx
+++ src/components/features/Menu/Credits/Credits.jsx
@@ -1,9 +1,12 @@
 import "./Credits.css";
-
 import { useDispatch } from "react-redux";
 import { setGameState } from "../../game/gameSlice";
+import { useTranslation } from "react-i18next";
+
 export default function Credits() {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
   return (
     <div className="bg-outer-credits absolute z-20">
       <div
@@ -11,28 +14,20 @@ export default function Credits() {
         className="credits absolute z-20"
       >
         <div className=" w-full h-full bg-black opacity-50">
-          <h1 className="h-text absolute text-white text-2xl">Hakkında</h1>
+          <h1 className="h-text absolute text-white text-2xl">{t('credits')}</h1>
           <p className="p-text absolute text-white text-justify">
-            Tamamen açık kaynak "Hearthstone" Oyun şu anda Blizzard Entertainment 'Hearthstone' oyununa benzer
-            şekilde geliştirilmiştir fakat kartların davranışı bazı noktalarda
-            farklılıklar göstermektedir.
+            {t('thisGameIsFanMade')}
             <br />
             <br />
-            Oyuna yeterli talep olduğu durumda 'Hearthstone' kimliğinden ayrılıp
-            Çeşitli modlara sahip olabilen benzersiz bir oyun olabilecek şekilde
-            geliştirildi.
+            {t('thankYouFor')}
             <br />
             <br />
-            Oyun şu an için backend servislerinden yoğun bir şekilde
-            yararlanmıyor. Fakat socket mimarisi veya backend servisleri
-            kullanılarak oyunun sonucu büyük oranda geliştirilebilir. Oyunun
-            geliştirme süreci olumlu yorumlar olduğu durumda devam edecektir. 
+            {t('sendingMeTheCardPackages')}
             <br />
             <br />
-            Oyunun geliştirilmesi sırasında kullanılan teknolojiler: SOLID, React,
-            Redux, Tailwind CSS, Vite, TypeScript, I18N internationalization
+            {t('developmentTechnologies')}
             <br /> <br />
-            Bu oyun Engin Karataş tarafından geliştirildi.
+            {t('developer')}
           </p>
         </div>
       </div>
