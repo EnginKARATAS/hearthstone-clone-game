788df56412ece5eaad5582311e4b8c6b2326677d
Engin Karatas <enginkaratas99@gmail.com>
2024-11-08 17:27:33 +0300

    delete unnecessary classes

--- src/skillManagement/cast/heal/concrates/HealCard.ts
+++
@@ -1,8 +0,0 @@
-import { Card } from "../../../../types/Card";
-import { Healable } from "../abstracts/Healable";
-
-export class HealCard implements Healable {
-  heal(card: Card, value: number): void {
-    card.cardHealth += value;
-  }
-}
--- src/skillManagement/cast/heal/concrates/HealHero.ts
+++
@@ -1,8 +0,0 @@
-import { Card, InitialState } from "../../../../types/Card";
-import { Castable } from "../../abstract/Castable";
-
-export class HealHero implements Castable {
-  cast(state: InitialState, card: Card): void {
-    state.profile.player!.cardHealth += 3;
-  }
-}
