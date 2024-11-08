e7a477be540fc912b6b55569e97b97f225e1b3b4
Engin Karatas <enginkaratas99@gmail.com>
2024-11-08 23:05:06 +0300

    open allowImportingTsExtensions

--- src/components/features/hand/handSlice.ts
+++ src/components/features/hand/handSlice.ts
@@ -10,7 +10,7 @@ import {
 } from "./cardService.ts";
 import { resetGame } from "../game/gameSlice.js";
 import { Card, InitialState } from "../../../types/Card.js";
-import { SkillManager } from "../../../skillManagement/skillManager/concrate/SkillManager.js";
+import { SkillManager } from "../../../skillManagement/skillManager/concrate/SkillManager.ts";
 const skillManager = new SkillManager();
 const initialState: InitialState = {
   hand: {
