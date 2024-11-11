e1aa34e862d790375de1a090a8d459cc87e3d003
Engin Karatas <enginkaratas99@gmail.com>
2024-11-11 19:16:53 +0300

    update responsiveness  with listeners for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import GameBoard from "./components/GameBoard";
 import "./App.css";
 import "./styles/fonts.css";
 import RotateScreen from "./components/features/Menu/RotateScreen/RotateScreen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setCardBase, setProfile } from "./components/features/hand/handSlice";
 import { useDispatch } from "react-redux";
 import Menu from "./components/features/Menu/Menu/Menu";
 import FishOrnament from "./components/features/UI/FishOrnament";
+
 function App() {
   const dispatch = useDispatch();
-  const windowHeight = window.innerHeight;
-  const windowWidth = window.innerWidth;
-  useEffect(() => {}, [windowHeight, windowWidth]);
-  if (windowWidth <= windowHeight) return <RotateScreen />;
+  const [windowDimensions, setWindowDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    };
+    
+    window.addEventListener('resize', handleResize);
+    
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (windowDimensions.width <= windowDimensions.height) return <RotateScreen />;
 
   return (
     <>
