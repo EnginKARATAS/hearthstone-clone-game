a710d3b8b23a32d69eb76c27a2f28f537f73f976
Engin Karatas <enginkaratas99@gmail.com>
2024-11-19 16:14:33 +0300

    responsive mobile part 5 board cards p1

--- src/components/features/Card/Board/Board.jsx
+++ src/components/features/Card/Board/Board.jsx
@@ -5,22 +5,28 @@ import BoardCard from "./BoardCard";
 export default function Board({ player, position }) {
   const playerBoardCards = useSelector((state) => state.hand.board.player);
   const enemyBoardCards = useSelector((state) => state.hand.board.enemy);
- 
+
   return (
     <div>
       {player === "player" && playerBoardCards && (
         <div
           className="board-cards absolute flex player-board"
-          style={{ top: -495, left: 55 - playerBoardCards.length * 50 }}
+          style={{
+            top: window.innerHeight > 768 ? -495 : -246,
+            left:
+              window.innerHeight > 768
+                ? 55 - playerBoardCards.length * 50
+                : -40 - playerBoardCards.length * 50,
+          }}
         >
           {playerBoardCards.map((boardCard, i) => {
             return (
               <BoardCard
                 position={{
                   left: i * 100,
-                offset: 555,
-              }}
-              boardCard={boardCard}
+                  offset: 555,
+                }}
+                boardCard={boardCard}
                 key={boardCard.cardId}
               />
             );
@@ -30,7 +36,13 @@ export default function Board({ player, position }) {
       {player === "enemy" && enemyBoardCards && (
         <div
           className="board-cards absolute flex enemy-board"
-          style={{ top: 310, left: 55 - enemyBoardCards.length * 50}}
+          style={{
+            top: window.innerHeight > 768 ? 310 : 180,
+            left:
+              window.innerHeight > 768
+                ? 55 - enemyBoardCards.length * 50
+                : -305 - enemyBoardCards.length * 50,
+          }}
         >
           {enemyBoardCards.map((enemyBoardCard, i) => {
             return (
--- src/components/features/UI/Profile/ClientProfile/ClientProfile.jsx
+++ src/components/features/UI/Profile/ClientProfile/ClientProfile.jsx
@@ -10,30 +10,42 @@ import { useSelector } from "react-redux";
 export default function ClientProfile({ player, position }) {
   const profile = useSelector((state) => state.hand.profile.player);
   return (
-    <div className="absolute client-profile"
-    style={{
-      bottom: window.innerHeight < 768 ? 415:-44,
-      left: window.innerHeight < 768 ? 388:631,
-    }}
+    <div
+      className="absolute client-profile"
+      style={{
+        bottom: window.innerHeight < 768 ? 415 : -44,
+        left: window.innerHeight < 768 ? 388 : 631,
+      }}
     >
       <Board player="player" position={{ top: -497, left: 150 }} />
       <RemainingCards player="player" position={{ top: -545, right: -558 }} />
       <Profile
         className="enemy-profile"
         img={{ pack: profile.cardPack }}
-        position={{ 
+        position={{
           left: window.innerHeight > 768 ? 10 : -69,
           bottom: window.innerHeight > 768 ? 227 : 50,
         }}
         player="player"
       />
-      <ManaBox position={{ top: -151, left: 253 }} player="player" />
+      <ManaBox
+        position={{
+          top: window.innerHeight > 768 ? -151 : -66,
+          left: window.innerHeight > 768 ? 253 : 52,
+        }}
+        player="player"
+      />
       <Hand
         position={{ bottom: -15, left: -580 }}
         player={player}
         className="hand"
       />
-      <SingleGameCard position={{ bottom: 280, left: -600 }} />
+      <SingleGameCard
+        position={{
+          bottom: window.innerHeight > 768 ? 280 : 40,
+          left: window.innerHeight > 768 ? -600 : -420,
+        }}
+      />
     </div>
   );
 }
